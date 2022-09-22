8dea0f583f48ad8be71a9062a274206832146171
Golik Andrii <Golik.Andrii@gmail.com>
2022-09-22 13:00:22 +0200

    connect to mongoDB + install mongoose

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyBarser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
 const authRoutes = require('./routes/auth')
 const categoryRoutes = require('./routes/category')
 const todosRoutes = require('./routes/todos')
+const keys = require('./config/keys')
 
 const app = express()
 
+mongoose.connect(keys.mongoURI)
+    .then(()=>console.log('MongoDB connected'))
+    .catch(error => console.log(error))
+
+app.use(require('morgan')('dev'))
+app.use(require('cors')(''))
 app.use(bodyBarser.urlencoded({extended: false}))
 app.use(bodyBarser.json())
 
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://admin:1234567890@cluster0.mmxjppd.mongodb.net/test'
+}
