1a27d370d0e0bf1a1c5eb0ee500a30de77361537
Golik Andrii <Golik.Andrii@gmail.com>
2022-09-25 19:16:27 +0200

    Change register-page.component

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,31 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../shared/services/auth.service";
 import {logMessages} from "@angular-devkit/build-angular/src/builders/browser-esbuild/esbuild";
 import {error} from "@angular/compiler-cli/src/transformers/util";
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   // @ts-ignore
   form: FormGroup
+  // @ts-ignore
+  aSub: Subscription
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {
+
+  }
 
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(3)])
     })
-  }
-  onSubmit(){
-    this.auth.login(this.form.value).subscribe(
-      ()=>console.log(this.form.value)
-    )
 
+    this.route.queryParams.subscribe((params:Params)=>{
+      if (params['registered']){
+// Now you can enter
+      } else if(params['accessDenied']){
+        //You need Authorisation
+      }
+    })
   }
+  ngOnDestroy() {
+    if (this.aSub){
+
+      this.aSub.unsubscribe()
+    }
+  }
+
+  onSubmit(){
+    this.form.disable()
+   this.aSub = this.auth.login(this.form.value).subscribe(
+      ()=> this.router.navigate(['/overview'])
+    )
+  }
+
 }
--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -2,12 +2,15 @@ import {Injectable} from "@angular/core"
 import {User} from "../interfaces";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import { tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService{
+
+  private token = 'null'
 
   constructor(private http: HttpClient) {
   }
@@ -16,7 +19,30 @@ export class AuthService{
 
   login(user: User): Observable<{token: string}>{
     return this.http.post<{token: string}>('/todo/auth/login', user)
+      .pipe(
+        tap(
+          ({token})=>{
+            localStorage.setItem('auth-token', token)
+            this.setToken(token)
+          }
+        )
+      )
   }
 
+  setToken(token: string){
+    this.token = token
+  }
+  getToken():string {
+    return this.token
+  }
+
+  isAuthenticated(): boolean{
+    return !!this.token
+  }
+
+  loguot(){
+    this.setToken('null')
+    localStorage.clear()
+  }
 
 }
