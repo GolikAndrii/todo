0504ce2de68dd350f340ee5f3bad4d64b6a2d489
Golik_Andrii <Golik.Andrii@gmail.com>
2022-09-26 16:14:54 +0200

    CanActivate is Done!

--- client/src/app/shared/classes/auth.guard.ts
+++ client/src/app/shared/classes/auth.guard.ts
@@ -1,40 +1,33 @@
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  CanActivateChild,
-  Router,
-  RouterState,
-  RouterStateSnapshot
-} from "@angular/router";
-import {Observable, of} from "rxjs";
-import {Injectable} from "@angular/core";
-import {AuthService} from "../services/auth.service";
-
-@Injectable({
-  providedIn: 'root'
-})
-
-export class AuthGuard implements CanActivate, CanActivateChild {
-  constructor(private auth: AuthService) {
-  }
-
-  CanActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    if (this.auth.isAuthenticated()) {
-      return of(true)
-    } else {
-      this.router.navigate(['/login'], {
-        queryParams: {
-          accessDenied: true
-        }
-      })
-      return of(false)
-    }
-  }
-
-  CanActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.CanActivate(route, state)
-  }
-
-}
-
-
+// import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterState, RouterStateSnapshot} from "@angular/router";
+// import {Observable, of} from "rxjs";
+// import {Injectable} from "@angular/core";
+// import {AuthService} from "../services/auth.service";
+//
+// @Injectable({
+//   providedIn: 'root'
+// })
+//
+// export class AuthGuard implements CanActivate, CanActivateChild {
+//   constructor(private auth: AuthService) {
+//   }
+//
+//   CanActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+//     if (this.auth.isAuthenticated()) {
+//       return of(true)
+//     } else {
+//       this.router.navigate(['/login'], {
+//         queryParams: {
+//           accessDenied: true
+//         }
+//       })
+//       return of(false)
+//     }
+//   }
+//
+//   CanActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+//     return this.CanActivate(route, state)
+//   }
+//
+// }
+//
+//
