dfd27d2d4422c2ec23ccd9bad7e1d57aad1ab802
Golik Andrii <Golik.Andrii@gmail.com>
2022-09-26 13:56:16 +0200

    edit path for Categories

--- routes/category.js
+++ routes/category.js
@@ -5,11 +5,11 @@ const controller = require('../controlers/category')
 const passport = require('passport')
 
 //http://localhost:5000/todo/category/
-router.get('/todo/',passport.authenticate('jwt', {session: false}), controller.getAll )
-router.get('/todo/:categoryId', controller.getById)
-router.delete('/todo/:id', controller.remove )
-router.post('/todo/', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.create )
-router.patch('/todo/:id',  upload.single('image'), controller.update )
+router.get('/',passport.authenticate('jwt', {session: false}), controller.getAll )
+router.get('/:categoryId', controller.getById)
+router.delete('/:id', controller.remove )
+router.post('/', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.create )
+router.patch('/:id',  upload.single('image'), controller.update )
 
 
 module.exports = router
