f2df6fc2bf01d2124f319a7396cc0cde4276f8fa
Golik Andrii <Golik.Andrii@gmail.com>
2022-09-27 14:05:55 +0200

    Edit app routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {SiteLayoutComponent} from "./shared/layouts/site-layout/site-layout.comp
 import {RegisterPageComponent} from "./register-page/register-page.component";
 import {OverviewPageComponent} from "./overview-page/overview-page.component";
 import {CategoriesPageComponent} from "./categories-page/categories-page.component";
+import {AlltodoPageComponent} from "./alltodo-page/alltodo-page.component";
 // import {AuthGuard} from "./shared/classes/auth.guard";
 
 if (localStorage.getItem('')) {
@@ -27,7 +28,8 @@ const routes: Routes = [
     // path: "", component: SiteLayoutComponent, canActivate:[AuthGuard], children: [
 
       {path: 'overview', component: OverviewPageComponent},
-      {path: 'categories', component: CategoriesPageComponent}
+      {path: 'categories', component: CategoriesPageComponent},
+      {path: 'alltodo', component: AlltodoPageComponent}
     ]
   }
 ];
