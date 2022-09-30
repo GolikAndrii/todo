3e11ebd45e0b243b9c88f212d39dec4f21542384
Golik Andrii <Golik.Andrii@gmail.com>
2022-09-30 14:17:57 +0200

    Edited page for #todo

--- client/src/app/alltodo-page/alltodo-page.component.ts
+++ client/src/app/alltodo-page/alltodo-page.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {Category} from "../shared/interfaces";
 
 @Component({
   selector: 'app-alltodo-page',
   templateUrl: './alltodo-page.component.html',
   styleUrls: ['./alltodo-page.component.css']
 })
-export class AlltodoPageComponent implements OnInit {
 
-  constructor() { }
+export class AlltodoPageComponent implements OnInit {
+// @ts-ignore
+  @ViewChild('input') inputRef: ElementRef
+
+  // @ts-ignore
+  form: FormGroup
+  isNew = true
+  // @ts-ignore
+  image: File
+  // @ts-ignore
+  imagePreview: string | ArrayBuffer | null = ''
+  imagePreviewEdit: string | undefined = ''
+  // @ts-ignore
+  category: Category
 
   ngOnInit(): void {
+  }
+
+  triggerClick() {
+    this.inputRef.nativeElement.click()
+  }
+
+  onFileUpload(event: any) {
+    const file = event.target.files[0]
+    this.image = file
+
+    const reader = new FileReader()
+    reader.onload = () => {
+      this.imagePreview = reader.result
+    }
+    reader.readAsDataURL(file)
+  }
+  onSubmit() {
+    let obs$
+    if (this.isNew) {
+      // create
+
+    } else {
+      //update
+
+    }
+
   }
 
 }
